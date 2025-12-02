3384c4103aa6809347da54a0db2c753d28b4f3d5
Prashant Sharma <78917743+Prashancoder@users.noreply.github.com>
2025-12-02 12:30:22 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,115 +1,119 @@
-import { genToken } from "../configs/token.js"
-import validator from "validator"
+import { genToken } from "../configs/token.js";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import sendMail from "../configs/Mail.js";
 
-import bcrypt from "bcryptjs"
-import User from "../models/userModel.js"
-
-import sendMail from "../configs/Mail.js"
-
-
-export const signUp=async (req,res)=>{
- 
+export const signUp = async (req, res) => {
     try {
+        let { name, email, password, role } = req.body;
 
-        let {name,email,password,role}= req.body
-        let existUser= await User.findOne({email})
-        if(existUser){
-            return res.status(400).json({message:"email already exist"})
+        let existUser = await User.findOne({ email });
+        if (existUser) {
+            return res.status(400).json({ message: "email already exist" });
         }
-        if(!validator.isEmail(email)){
-            return res.status(400).json({message:"Please enter valid Email"})
+
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ message: "Please enter valid Email" });
         }
-        if(password.length < 8){
-            return res.status(400).json({message:"Please enter a Strong Password"})
+
+        if (password.length < 8) {
+            return res.status(400).json({ message: "Please enter a Strong Password" });
         }
-        
-        let hashPassword = await bcrypt.hash(password,10)
-        let user = await User.create({
-            name ,
-            email ,
-            password:hashPassword ,
-            role,
-           
-            })
-        let token = await genToken(user._id)
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
+
+        let hashPassword = await bcrypt.hash(password, 10);
+        let user = await User.create({ name, email, password: hashPassword, role });
+
+        let token = await genToken(user._id);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
             sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
-        })
-        return res.status(201).json(user)
+        });
+
+        return res.status(201).json(user);
 
     } catch (error) {
-        console.log("signUp error")
-        return res.status(500).json({message:`signUp Error ${error}`})
+        console.log("signUp error");
+        return res.status(500).json({ message: `signUp Error ${error}` });
     }
-}
+};
 
-export const login=async(req,res)=>{
+export const login = async (req, res) => {
     try {
-        let {email,password}= req.body
-        let user= await User.findOne({email})
-        if(!user){
-            return res.status(400).json({message:"user does not exist"})
+        let { email, password } = req.body;
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "user does not exist" });
         }
-        let isMatch =await bcrypt.compare(password, user.password)
-        if(!isMatch){
-            return res.status(400).json({message:"incorrect Password"})
+
+        let isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "incorrect Password" });
         }
-        let token =await genToken(user._id)
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+
+        let token = await genToken(user._id);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
-        })
-        return res.status(200).json(user)
+        });
+
+        return res.status(200).json(user);
 
     } catch (error) {
-        console.log("login error")
-        return res.status(500).json({message:`login Error ${error}`})
+        console.log("login error");
+        return res.status(500).json({ message: `login Error ${error}` });
     }
-}
+};
 
-
-
-
-export const logOut = async(req,res)=>{
+export const logOut = async (req, res) => {
     try {
-        await res.clearCookie("token")
-        return res.status(200).json({message:"logOut Successfully"})
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        });
+
+        return res.status(200).json({ message: "logOut Successfully" });
+
     } catch (error) {
-        return res.status(500).json({message:`logout Error ${error}`})
+        return res.status(500).json({ message: `logout Error ${error}` });
     }
-}
+};
 
-
-export const googleSignup = async (req,res) => {
+export const googleSignup = async (req, res) => {
     try {
-        const {name , email , role} = req.body
-        let user= await User.findOne({email})
-        if(!user){
-            user = await User.create({
-            name , email ,role
-        })
+        const { name, email, role } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ name, email, role });
         }
-        let token =await genToken(user._id)
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        })
-        return res.status(200).json(user)
 
+        let token = await genToken(user._id);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.status(200).json(user);
 
     } catch (error) {
-        console.log(error)
-         return res.status(500).json({message:`googleSignup  ${error}`})
+        console.log(error);
+        return res.status(500).json({ message: `googleSignup  ${error}` });
     }
-    
-}
+};
 
 export const sendOtp = async (req,res) => {
     try {
