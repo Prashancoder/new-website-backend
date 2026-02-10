477fd7770d01f3b4203e0e44a5ec17fca4a5a71f
Prashancoder <ps985094@gmail.com>
2026-02-10 17:24:38 +0530

    aaaaa

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -17,9 +17,19 @@ router.get('/', getAllBlogs);
 router.get('/slug/:slug', getBlogBySlug);
 
 // Admin routes (protected)
-router.post('/', isAuth, createBlog);
-router.get('/admin/:id', isAuth, getBlogById);
-router.put('/:id', isAuth, updateBlog);
-router.delete('/:id', isAuth, deleteBlog);
+// router.post('/', isAuth, createBlog);
+// router.get('/admin/:id', isAuth, getBlogById);
+// router.put('/:id', isAuth, updateBlog);
+// router.delete('/:id', isAuth, deleteBlog);
+
+
+
+router.post('/', createBlog);
+router.get('/admin/:id', getBlogById);
+router.put('/:id', updateBlog);
+router.delete('/:id', deleteBlog);
+
+
+
 
 export default router;
